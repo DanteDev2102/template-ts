95719718d8a7970ea56d1bd88482d5c1ac50d62d
Jose Acosta <jdap.dmc@gmail.com>
2022-04-11 20:10:47 -0400

    del: http-terminator

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,14 @@
-import { createHttpTerminator } from 'http-terminator';
 import app from './app';
 import config from './config';
 import { connectDB } from './database';
 
-const server = app.listen(config.portServer, () => {
+app.listen(config.portServer, () => {
   console.log(`server listenin on ${config.hostServer}:${config.portServer}`);
   console.log('kill server press ctrl + c');
   connectDB(config.mongoURI);
 });
 
-const httpterminator = createHttpTerminator({ server });
-
 process.on('uncaughtException', error => {
-  console.log('uncaugth exception: ', error);
+  console.error('uncaugth exception: ', error);
   process.exit(1);
 });
-
-setTimeout(() => {
-  httpterminator.terminate();
-}, 1000);
